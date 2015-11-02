ae791ba3e4664e27ae7ca0b9a5dd1756bf58357e
purorus <ramadan.denny@gmail.com>
2015-11-02 10:47:19 +0700

    123456

---
+++ src/Parking/Kostumer.java
@@ -0,0 +1,14 @@
+package Parking;
+
+public class Kostumer {
+    String motor;
+    String tiketParkir;
+    String helm;
+
+    public void motor(){
+    System.out.println("Memarkirkan motor pada tempat yang telah disediakan");
+    }
+    public void tiketParkir(){
+        System.out.println("Memberikan Kepada tukang parkir");
+    }
+}
---
+++ src/Parking/TukangParkir.java
@@ -0,0 +1,14 @@
+package Parking;
+
+public class TukangParkir {
+    String tiketParkir;
+    String kasihTiketParkir;
+
+    public TukangParkir(String tiketParkir){
+        this.tiketParkir = tiketParkir;
+       // System.out.println(this.tiketParkir);
+    }
+    public void kasihTiketParkir(){
+        System.out.println("Memberikan Tiket Parkir Kepada Kostumer");
+    }
+}
